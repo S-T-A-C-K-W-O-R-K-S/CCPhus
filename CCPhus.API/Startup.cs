2f53c2269cd6eb5caf6d1e327b2a89103e51d1c2
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-09-16 15:18:25 +0100

    Connect Pomelo MySQL To Database

--- CCPhus.API/Startup.cs
+++ CCPhus.API/Startup.cs
@@ -27,7 +27,7 @@ namespace CCPhus.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseMySQL("ConnectionString"));
+            services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
