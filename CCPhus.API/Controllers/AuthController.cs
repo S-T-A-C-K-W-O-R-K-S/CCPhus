269eb25b17d13c597ffb307c0b09edb175489535
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-09-17 15:28:51 +0100

    Create User Registration Methods

---
+++ CCPhus.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CCPhus.API.Data;
+using CCPhus.API.DTOs;
+using CCPhus.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCPhus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDTO userForRegisterDTO)
+        {
+            if (await _repo.UserExists(userForRegisterDTO.Username).ConfigureAwait(false)) return BadRequest("User Already Exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDTO.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDTO.Password).ConfigureAwait(false);
+
+            return StatusCode(201);
+        }
+    }
+}
