6df327c6ac510139f428a10861ddd5238f36ff97
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-09-17 16:44:01 +0100

    Create Login Method & Add Authentication Middleware

--- CCPhus.API/DTOs/UserForLoginDTO.cs
+++ CCPhus.API/DTOs/UserForLoginDTO.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace CCPhus.API.DTOs
 {
-    public class UserForRegisterDTO
+    public class UserForLoginDTO
     {
         [Required(ErrorMessage = "Username Is Required")]
-        [StringLength(32, MinimumLength = 4, ErrorMessage = "Username Must Be Between 4 And 32 Characters")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Password Is Required")]
-        [StringLength(24, MinimumLength = 6, ErrorMessage = "Password Must Be Between 6 And 24 Characters")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
