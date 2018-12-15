d1f0453d0e054451aada68087c060f27073a9d65
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-12-15 14:01:56 +0000

    Add Seeder To Startup Class

--- CCPhus.API/Startup.cs
+++ CCPhus.API/Startup.cs
@@ -37,6 +37,7 @@ namespace CCPhus.API
             services.AddDbContext<DataContext>(x => x.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -54,7 +55,7 @@ namespace CCPhus.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Seed seeder)
         {
             if (env.IsDevelopment())
             {
@@ -82,6 +83,7 @@ namespace CCPhus.API
             }
 
             app.UseHttpsRedirection();
+            seeder.SeedRootUsers();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
             app.UseMvc();
