7145b67bf5fa79437c1a3c9fe2c43d2a55817d42
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2018-12-15 13:36:19 +0000

    Implement Seed Data

---
+++ CCPhus.API/Data/Seed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using CCPhus.API.Models;
+using Newtonsoft.Json;
+
+namespace CCPhus.API.Data
+{
+    public class Seed
+    {
+        private readonly DataContext _context;
+
+        public Seed(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedRootUsers()
+        {
+            var rootUsersData = System.IO.File.ReadAllText("Data/SeedRootUsersData.json");
+            var rootUsers = JsonConvert.DeserializeObject<List<User>>(rootUsersData);
+            foreach (var rootUser in rootUsers)
+            {
+                
+            }
+        }
+    }
+}
