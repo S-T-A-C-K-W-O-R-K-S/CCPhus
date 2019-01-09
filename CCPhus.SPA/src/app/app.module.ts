491d3f2bf98a417b0c25a7ac73acbc759ad129d1
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2019-01-09 23:35:04 +0000

    Add Tabset On Member Detail Page

--- CCPhus.SPA/src/app/app.module.ts
+++ CCPhus.SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -50,7 +50,8 @@ export function tokenGetter() {
             whitelistedDomains: ['localhost:5000', 'localhost:5001'],
             blacklistedRoutes: ['localhost:5000/api/auth', 'localhost:5001/api/auth']
          }
-      })
+      }),
+      TabsModule.forRoot()
    ],
    providers: [
       AuthService,
