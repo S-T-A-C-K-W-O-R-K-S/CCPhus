8be7e5df4cb205e085e90092584dd888b13c73e3
Vlad-Alexandru Tănăsescu <vatanasescu@outlook.com>
2019-01-24 23:33:30 +0000

    Create User Avatar Gallery

--- CCPhus.SPA/src/app/member-detail/member-detail.component.ts
+++ CCPhus.SPA/src/app/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from '../_models/user';
 import { UserService } from '../_services/user.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,6 +13,8 @@ import { ActivatedRoute } from '@angular/router';
 
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryAvatars: NgxGalleryImage[];
 
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
@@ -19,5 +22,33 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+
+    this.galleryOptions = [
+      {
+        width: '500px',
+        height: '500px',
+        imagePercent: 100,
+        thumbnailsColumns: 4,
+        imageAnimation: NgxGalleryAnimation.Slide,
+        preview: false
+      }
+    ];
+
+    this.galleryAvatars = this.getAvatars();
+  }
+
+  getAvatars() {
+    const avatarURLs = [];
+    for (let i=0; i < this.user.avatars.length; i++) {
+      avatarURLs.push(
+        {
+          small: this.user.avatars[i].url,
+          medium: this.user.avatars[i].url,
+          big: this.user.avatars[i].url
+        }
+      )
+    }
+
+    return avatarURLs;
   }
 }
